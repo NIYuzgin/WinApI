264b649b97a6f6ffd63d2c4ee61c112820ea86f1
Николай Немов <prost91@outlook.com>
2025-11-16 17:44:54 +0300

    Copy DONE

--- WinApI/main.cpp
+++ WinApI/main.cpp
@@ -5,8 +5,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	/*
-	MessageBox
+	/*MessageBox
 	(
 		NULL, 
 		"Hello WinAPI!", 
@@ -18,26 +17,24 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		| MB_SYSTEMMODAL
 		| MB_RIGHT
 	);
-	*/
+	
 	// MB_ - MessageBox;
+*/
 
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
-
 	return 0;
 }
 
-BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)	// CALLBACK - конвенция вызова функции
 {
 	switch (uMsg)
 	{
 	case WM_INITDIALOG: // Выполняется один раз при запуске окна
-	
 	{	
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 		
 		//SetFocus(GetDlgItem(hwnd, IDC_EDIT_LOGIN));
-		
 	}	
 		break;
 	case WM_COMMAND:	// Обрабатывает команды с клавиатуры и мыши
@@ -51,7 +48,6 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-
 		}	
 			break;
 		
