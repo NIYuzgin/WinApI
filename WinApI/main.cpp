f8b159a8e1f53c1d9cfc28a659c646e80bc47c04
Николай Немов <prost91@outlook.com>
2025-11-16 08:31:19 +0300

    Add project files.

---
+++ WinApI/main.cpp
@@ -0,0 +1,70 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	/*
+	MessageBox
+	(
+		NULL, 
+		"Hello WinAPI!", 
+		"MessageBox", 
+		MB_YESNOCANCEL 
+		| MB_ICONERROR 
+		| MB_HELP 
+		| MB_DEFBUTTON3 
+		| MB_SYSTEMMODAL
+		| MB_RIGHT
+	);
+	*/
+	// MB_ - MessageBox;
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG: // Выполняется один раз при запуске окна
+	
+	{	
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		
+		//SetFocus(GetDlgItem(hwnd, IDC_EDIT_LOGIN));
+		
+	}	
+		break;
+	case WM_COMMAND:	// Обрабатывает команды с клавиатуры и мыши
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+
+		}	
+			break;
+		
+		case IDOK:
+			MessageBox(NULL, "Была нажата кнопка 'OK'", "Info", MB_OK | MB_ICONINFORMATION);
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+	case WM_CLOSE:		// Выполняется при нажатии кнопки 'x'
+		EndDialog(hwnd,0);
+	}
+	return FALSE;
+}
