2971d3114aa8259664d06722e01f33d6073656f4
Николай Немов <prost91@outlook.com>
2025-11-26 20:12:37 +0300

    Save ListBox to file DONE

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -8,6 +8,7 @@ CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "List","Box" };
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgProcEdit(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+VOID SaveList(HWND hwnd, CONST CHAR filename[]);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -74,6 +75,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 		
 		case IDCANCEL:
+			SaveList(hwnd, "list.txt");
 			EndDialog(hwnd, 0);
 			break;
 		}
@@ -98,7 +100,9 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	break;*/
 	
 	case WM_CLOSE:
+		SaveList(hwnd, "list.txt");
 		EndDialog(hwnd, 0);
+		
 		//break;
 	}
 	return FALSE;
@@ -193,5 +197,41 @@ BOOL CALLBACK DlgProcEdit(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		EndDialog(hwnd, 0);
 	}
 	return FALSE;
+}
+
+VOID SaveList(HWND hwnd, const CHAR filename[])
+{
+	CONST INT SIZE = 32768;
+	CHAR sz_buffer[SIZE] = {};
+	
+	HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+	INT n = SendMessage(hList, LB_GETCOUNT, 0, 0);
+
+	for (int i = 0; i < n; i++)
+	{
+		CHAR sz_item[256] = {};
+		SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_item);
+		strcat(sz_buffer, "\n");
+		strcat(sz_buffer, sz_item);
+		/*
+		lstrcat(sz_buffer, "\n");
+		lstrcat(sz_buffer, sz_item);
+		*/
+
+	}
+
+	HANDLE hFile = CreateFile
+	(
+		filename,
+		GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+	);
+	DWORD dwBytesWritten = 0;
+	WriteFile(hFile, sz_buffer, strlen(sz_buffer), &dwBytesWritten, NULL);
+	CloseHandle(hFile);
 }
 
