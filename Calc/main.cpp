44d85e25072c01c932ff8cc7aafc875775a81463
Николай Немов <prost91@outlook.com>
2025-12-08 19:19:10 +0300

    Calc

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -227,6 +227,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 	case WM_COMMAND:
 	{
+		//SetFocus(hwnd);
 		HWND hEdit = GetDlgItem(hwnd, IDC_DISPLAY);
 		CONST INT SIZE = 256;
 		CHAR sz_buffer[SIZE] = {};
